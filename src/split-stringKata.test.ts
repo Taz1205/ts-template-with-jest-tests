b156a579c2d080db453115f77b15a3456b1a465a
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-07 12:49:49 +0100

    feat: implemented split-string kata

---
+++ src/split-stringKata.test.ts
@@ -0,0 +1,11 @@
+import { splitString } from "./split-stringKata";
+
+describe("split string kata", () => {
+    it("should split a string", () => {
+      expect(splitString("")).toBe("");
+    });
+    // it("should return 5 for add(2,3)", () => {
+    //   expect(add(2, 3)).toBe(5);
+    // });
+  });
+  
